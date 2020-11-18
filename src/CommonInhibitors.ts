92122ebaf2a47c31da7f485860617558bf7e0dfd
Alistair Smith <alistairsmith01@gmail.com>
2020-11-18 23:03:05 +0000

    feat: add another inhibitor

--- src/CommonInhibitors.ts
+++ src/CommonInhibitors.ts
@@ -1,19 +1,50 @@
-import { Message } from "discord.js";
+import { Message, Permissions } from "discord.js";
 
 /**
  * A function that will halt execution if an error is thrown
  */
 export type Inhibitor = (message: Message) => void | never;
 
-export const CommonInhibitors: Record<"guildsOnly" | "noBots", Inhibitor> = {
-  noBots: (message: Message) => {
-    if (message.author.bot) {
-      throw new Error("This command cannot be used by bots");
-    }
-  },
-  guildsOnly: (message: Message) => {
-    if (!message.guild) {
-      throw new Error("This command can only be used in a server");
-    }
-  },
+/**
+ * Make this command executable by humans only
+ * @param message
+ */
+export const noBots: Inhibitor = (message) => {
+  if (message.author.bot) {
+    throw new Error("This command cannot be used by bots");
+  }
+};
+
+/**
+ * Make this command run in a guild only
+ * @param message
+ */
+export const guildsOnly: Inhibitor = (message) => {
+  if (!message.guild) {
+    throw new Error("This command can only be used in a server");
+  }
+};
+
+/**
+ * Make this command executable by guild admins only
+ * @param message
+ */
+export const guildAdminsOnly: Inhibitor = (message) => {
+  if (!message.guild) {
+    throw new Error("This command can only be used in a server");
+  }
+
+  if (!message.member) {
+    throw new Error("Could not find member of message");
+  }
+
+  if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+    throw new Error("You do not have permission to use this command");
+  }
+};
+
+export const CommonInhibitors = {
+  noBots,
+  guildsOnly,
+  guildAdminsOnly,
 } as const;
